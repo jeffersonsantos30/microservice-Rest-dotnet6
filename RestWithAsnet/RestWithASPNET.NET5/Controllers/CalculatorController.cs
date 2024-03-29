e2145b212660658102a42fba36021ffc7a150615
jeffersonsantos30 <jeferson29santos@gmail.com>
2024-03-28 21:32:37 -0300

    refactored code-calculator

---
+++ RestWithAsnet/RestWithASPNET.NET5/Controllers/CalculatorController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNET.NET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{fristNumber}/{secondNumber}")]
+        public IActionResult Get(string fristNumber,string secondNumber)
+        {
+            if (IsNumeric (fristNumber) && IsNumeric(secondNumber)) ;
+            {
+                var sum = ConvertToDecimal(fristNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private int ConvertToDecimal(string secondNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsNumeric(string fristNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
